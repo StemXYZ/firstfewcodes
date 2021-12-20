831aa3459a904889a3a251efe188e1b48ea76a21
Stefan <96190191+StemXYZ@users.noreply.github.com>
2021-12-20 12:57:47 +0100

    Add files via upload

---
+++ C++/Codes from Book_1/LISTING 1.1 Hello.cpp
@@ -0,0 +1,10 @@
+#include <cstdio> // small
+#include <iostream> // big
+
+int main() 
+{
+	printf("hello world");  //diz is in both libs
+	std::cout<<std::endl;   //diz is in iostream
+	std::cout << " Tony is a " << std::endl; //diz also only in iostream
+	return 0;
+}

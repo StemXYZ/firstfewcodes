218f58d160aef9bece1165b0ec1c423799ca99af
Stefan <96190191+StemXYZ@users.noreply.github.com>
2021-12-21 14:31:48 +0100

    Update PrimeCounter++.cpp

--- C++/PublicProjects/PrimeCounter++.cpp
+++ C++/PublicProjects/PrimeCounter++.cpp
@@ -2,24 +2,6 @@
 
 using namespace std;
 
-int a()
-{
-    int n = 1, i, m = 0, flag = 0;
-    m = n / 2;
-    for (i = 2; i <= m; i++)
-    {
-        if (n % i == 0)
-        {
-            cout << "o " << endl;
-            flag = 1;
-            break;
-        }
-    }
-    if (flag == 0)
-        cout << "i " << endl;
-    return 0;
-}
-
 int main() 
 {
     int n = 1;
